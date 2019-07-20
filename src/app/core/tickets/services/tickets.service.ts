c9f1b380f8312a060b6af7dc9b8bf5f9a4a07b81
rstriana04 <robertsty99@gmail.com>
2019-07-20 11:24:59 -0500

    Changes in tickets.component

--- src/app/core/tickets/services/tickets.service.ts
+++ src/app/core/tickets/services/tickets.service.ts
@@ -1,9 +1,37 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { environment } from '../../../../environments/environment';
+import { Ticket } from '../models/Ticket';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TicketsService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAllTickets(): Observable<Ticket[]> {
+    return this.http.get<Ticket[]>(`${ environment.apiUrl }tickets`, {
+      observe: 'response',
+      responseType: 'json'
+    }).pipe(
+      map(tickets => tickets.body)
+    );
+  }
+
+  createTicket(ticket: Ticket): Observable<any> {
+    return this.http.post(`${ environment.apiUrl }tickets`, ticket, {
+      observe: 'response',
+      responseType: 'json'
+    });
+  }
+
+  updateTicket(ticket: Ticket): Observable<any> {
+    return this.http.put(`${ environment.apiUrl }tickets`, ticket, {
+      observe: 'response',
+      responseType: 'json'
+    });
+  }
 }
--- src/app/core/tickets/tickets.component.ts
+++ src/app/core/tickets/tickets.component.ts
@@ -1,5 +1,7 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatPaginator } from '@angular/material';
+import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 import { auditTime, distinctUntilChanged, map, switchMap } from 'rxjs/operators';
 import { Airplanes } from '../airplanes/models/Airplanes';
@@ -8,6 +10,8 @@ import { Flights } from '../flights/models/Flights';
 import { FlightsService } from '../flights/services/flights.service';
 import { Passenger } from '../passenger/models/Passenger';
 import { PassengerService } from '../passenger/services/passenger.service';
+import { Ticket } from './models/Ticket';
+import { TicketsService } from './services/tickets.service';
 
 @Component({
   selector: 'fly-tickets',
@@ -17,6 +21,8 @@ import { PassengerService } from '../passenger/services/passenger.service';
 })
 export class TicketsComponent implements OnInit {
   isLinear = false;
+  displayedColumns: string[] = ['valorTiquete', 'ivaTiquete', 'descuentoTiquete', 'pasajero', 'vuelo', 'avion', 'edit'];
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
   ticketForm: FormGroup;
   passengers$: Observable<Passenger[]>;
   filteredPassengers$: Observable<Passenger[]>;
@@ -24,11 +30,14 @@ export class TicketsComponent implements OnInit {
   filteredAirplanes$: Observable<Airplanes[]>;
   flights$: Observable<Flights[]>;
   filteredFlights$: Observable<Flights[]>;
-
+  tickets$: Observable<Ticket[]>;
+  private iva = 0.19;
   constructor(
     private passengerService: PassengerService,
     private airplanesService: AirplanesService,
-    private flightsServices: FlightsService
+    private flightsServices: FlightsService,
+    private ticketsService: TicketsService,
+    private toastrService: ToastrService
   ) { }
 
   ngOnInit() {
@@ -36,6 +45,7 @@ export class TicketsComponent implements OnInit {
     this.getAllPassengers();
     this.getAllAirplanes();
     this.getAllFlights();
+    this.tickets$ = this.ticketsService.getAllTickets();
   }
 
   private initTicketForm() {
@@ -79,9 +89,7 @@ export class TicketsComponent implements OnInit {
     );
   }
 
-  public registerTicket(ticketForm: FormGroup) {
-    console.log(ticketForm.value);
-  }
+
 
   private filterPassengers(value: string): Observable<Passenger[]> {
     if ( value ) {
@@ -126,10 +134,44 @@ export class TicketsComponent implements OnInit {
   }
 
   public setPassengerToControl(passenger: Passenger) {
-    console.log(passenger);
+    this.ticketForm.get('passengers').setValue(passenger);
+
   }
 
   public setFlightsToControl(flight: Flights) {
-    console.log(flight);
+    this.ticketForm.get('flights').setValue(flight);
+  }
+
+  public setAirplanesToControl(airplane: Airplanes) {
+    this.ticketForm.get('airplanes').setValue(airplane);
+  }
+
+  public registerTicket(ticketForm: FormGroup) {
+    if ( ticketForm.valid ) {
+      this.ticketsService.createTicket(ticketForm.value).subscribe(resp => {
+        if ( resp.status === 200 ) {
+          this.toastrService.success('Ticket Creado Correctamente', '¡Correcto!');
+          this.ticketForm.reset();
+          this.refresh();
+        } else {
+          this.toastrService.error('Ocurrio un error creando el ticket', '¡Ooops...!');
+        }
+      });
+    } else {
+      this.toastrService.info('Complete el formulario', '¡Ooops...!');
+    }
+  }
+
+  public calculateIva($event: any) {
+    const iva = ( $event.target.value * this.iva );
+    this.ticketForm.get('ivaTiquete').setValue(iva);
+  }
+
+  private refresh() {
+    this.tickets$ = this.ticketsService.getAllTickets();
+  }
+
+  public showEditTicket(id: any) {
+    
   }
 }
